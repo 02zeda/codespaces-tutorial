f600e960f0d8b35998280ebe645970cbe3e16845
Zeth Danielsson <89939332+02zeda@users.noreply.github.com>
2024-10-06 07:40:14 +0000

    Copilot Changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Zeth Danielsson",
+  title: "Software Engineering student",
+  email: "zethdanielsson@gmail.com",
+  gitHub: "02zeda",
+  instagram: "",
+  linkedIn: "Zeth Danielsson",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
